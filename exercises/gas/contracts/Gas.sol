7b4087a52b7d6eeff11049786a3d779f79e7d01c
damienteo <damien.teo.ks@gmail.com>
2023-02-26 22:54:17 +0800

    Minor updates

--- exercises/gas/contracts/Gas.sol
+++ exercises/gas/contracts/Gas.sol
@@ -16,6 +16,14 @@ pragma solidity ^0.8.17; // Update minor version to 17 from 0, actually decrease
 error NotAdminOrOwner();
 error NotWhiteListed();
 
+error InsufficientBalance();
+error NameLengthError();
+
+error InvalidAmount();
+error InvalidId();
+error InvalidAddress();
+error InvalidTier();
+
 contract GasContract {
     // Remove un-necessary declarations as 0
     // Mainly decreases deployment cost
@@ -73,8 +81,6 @@ contract GasContract {
 
     mapping(address => ImportantStruct) public whiteListStruct;
 
-    event AddedToWhitelist(address userAddress, uint256 tier);
-
     modifier onlyAdminOrOwner() {
         address senderOfTx = msg.sender;
         // Simplified error check
@@ -126,6 +132,7 @@ contract GasContract {
     //     _;
     // }
 
+    event AddedToWhitelist(address userAddress, uint256 tier);
     event supplyChanged(address indexed, uint256 indexed);
     event Transfer(address recipient, uint256 amount);
     event PaymentUpdated(
@@ -154,16 +161,6 @@ contract GasContract {
         }
     }
 
-    // Changed to 'external view' from 'public payable'
-    // Decreases deployment cost by 0.2%
-    function getPaymentHistory()
-        external
-        view
-        returns (History[] memory paymentHistory_)
-    {
-        return paymentHistory;
-    }
-
     // Remove declaration of admin variable
     // Increases deployment cost by ~1000
     // But decreases updatePayment and addToWhiteList by ~200
@@ -179,23 +176,51 @@ contract GasContract {
         return false;
     }
 
-    // Removal of balance variable actually decreased deployment cost by ~1000
-    // Changing to external did not affect deployment cost
-    function balanceOf(address _user) external view returns (uint256) {
-        // uint256 balance = balances[_user];
-        return balances[_user];
-    }
+    function transfer(
+        address _recipient,
+        uint256 _amount,
+        string calldata _name
+    ) external returns (bool) {
+        // Not declaring new variable decreases gas cost by 15
+        // address senderOfTx = msg.sender;
 
-    function getTradingMode() public pure returns (bool) {
-        // Nothing changes tradeFlag and dividendFlag
-        return true;
-        // bool mode = false;
-        // if (tradeFlag == 1 || dividendFlag == 1) {
-        //     mode = true;
-        // } else {
-        //     mode = false;
+        // Changing to custom errors decrease deployment cost by 0.3%
+        // require(
+        //     balances[senderOfTx] >= _amount,
+        //     "Gas Contract - Transfer function - Sender has insufficient Balance"
+        // );
+        // require(
+        //     bytes(_name).length < 9,
+        //     "Gas Contract - Transfer function -  The recipient name is too long, there is a max length of 8 characters"
+        // );
+
+        if (balances[msg.sender] < _amount) revert InsufficientBalance();
+        if (bytes(_name).length > 8) revert NameLengthError();
+
+        balances[msg.sender] -= _amount;
+        balances[_recipient] += _amount;
+        emit Transfer(_recipient, _amount);
+
+        Payment memory payment;
+        payment.admin = address(0);
+        payment.adminUpdated = false;
+        payment.paymentType = PaymentType.BasicPayment;
+        payment.recipient = _recipient;
+        payment.amount = _amount;
+        payment.recipientName = _name;
+        payment.paymentID = ++paymentCounter;
+        payments[msg.sender].push(payment);
+
+        // tradePercent is always 12
+        // status[0] will always be true
+        // Reduces deployment cost by 0.1%
+        // Reduces gas cost by 3.5k
+        // bool[] memory status = new bool[](tradePercent);
+        // for (uint256 i = 0; i < tradePercent; i++) {
+        //     status[i] = true;
         // }
-        // return mode;
+        // return (status[0] == true);
+        return true;
     }
 
     // returns (bool status_, bool tradeMode_)
@@ -215,71 +240,32 @@ contract GasContract {
         // return ((status[0] == true), _tradeMode);
     }
 
-    function getPayments(address _user)
-        public
-        view
-        returns (Payment[] memory payments_)
-    {
-        require(
-            _user != address(0),
-            "Gas Contract - getPayments function - User must have a valid non zero address"
-        );
-        return payments[_user];
-    }
-
-    function transfer(
-        address _recipient,
-        uint256 _amount,
-        string calldata _name
-    ) public returns (bool status_) {
-        address senderOfTx = msg.sender;
-        require(
-            balances[senderOfTx] >= _amount,
-            "Gas Contract - Transfer function - Sender has insufficient Balance"
-        );
-        require(
-            bytes(_name).length < 9,
-            "Gas Contract - Transfer function -  The recipient name is too long, there is a max length of 8 characters"
-        );
-        balances[senderOfTx] -= _amount;
-        balances[_recipient] += _amount;
-        emit Transfer(_recipient, _amount);
-        Payment memory payment;
-        payment.admin = address(0);
-        payment.adminUpdated = false;
-        payment.paymentType = PaymentType.BasicPayment;
-        payment.recipient = _recipient;
-        payment.amount = _amount;
-        payment.recipientName = _name;
-        payment.paymentID = ++paymentCounter;
-        payments[senderOfTx].push(payment);
-        bool[] memory status = new bool[](tradePercent);
-        for (uint256 i = 0; i < tradePercent; i++) {
-            status[i] = true;
-        }
-        return (status[0] == true);
-    }
-
     function updatePayment(
         address _user,
         uint256 _ID,
         uint256 _amount,
         PaymentType _type
     ) public onlyAdminOrOwner {
-        require(
-            _ID > 0,
-            "Gas Contract - Update Payment function - ID must be greater than 0"
-        );
-        require(
-            _amount > 0,
-            "Gas Contract - Update Payment function - Amount must be greater than 0"
-        );
-        require(
-            _user != address(0),
-            "Gas Contract - Update Payment function - Administrator must have a valid non zero address"
-        );
+        // Changing to custom error reduces deplyment cost by 0.4%
+        // require(
+        //     _ID > 0,
+        //     "Gas Contract - Update Payment function - ID must be greater than 0"
+        // );
+        // require(
+        //     _amount > 0,
+        //     "Gas Contract - Update Payment function - Amount must be greater than 0"
+        // );
+        // require(
+        //     _user != address(0),
+        //     "Gas Contract - Update Payment function - Administrator must have a valid non zero address"
+        // );
 
-        address senderOfTx = msg.sender;
+        if (_amount == 0) revert InvalidAmount();
+        if (_ID == 0) revert InvalidId();
+        if (_user == address(0)) revert InvalidAddress();
+
+        // Not declaring senderOfTx reduces gas cost by 4
+        // address senderOfTx = msg.sender;
 
         for (uint256 ii = 0; ii < payments[_user].length; ii++) {
             if (payments[_user][ii].paymentID == _ID) {
@@ -290,7 +276,7 @@ contract GasContract {
                 // bool tradingMode = getTradingMode();
                 addHistory(_user);
                 emit PaymentUpdated(
-                    senderOfTx,
+                    msg.sender,
                     _ID,
                     _amount,
                     payments[_user][ii].recipientName
@@ -303,32 +289,52 @@ contract GasContract {
         public
         onlyAdminOrOwner
     {
-        require(
-            _tier < 255,
-            "Gas Contract - addToWhitelist function -  tier level should not be greater than 255"
-        );
+        // Using custom error here reduces deployment cost by 0.2%
+        //       require(
+        //     _tier < 255,
+        //     "Gas Contract - addToWhitelist function -  tier level should not be greater than 255"
+        // );
+        if (_tier > 254) revert InvalidTier();
         whitelist[_userAddrs] = _tier;
         if (_tier > 3) {
-            whitelist[_userAddrs] -= _tier;
+            // Not sure why there us a need to decrease tier
+            // if it will be set to 3 in the end
+            // Same for rest of if/else
+            // decreases deployment cost by 0.2%
+            // gas cost by 1k
             whitelist[_userAddrs] = 3;
-        } else if (_tier == 1) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 1;
+            // following two if/else can be combined
+            // } else if (_tier == 1) {
+            //     whitelist[_userAddrs] = 1;
+            // } else if (_tier > 0 && _tier < 3) {
+            //     whitelist[_userAddrs] = 2;
+            // }
+            // decreases gas cost by 16,
+            // decreases deployment cost by 18k
         } else if (_tier > 0 && _tier < 3) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 2;
+            whitelist[_userAddrs] = _tier;
         }
+        // Removing declaration of this variable will decrease deployment cost
+        // But increase running cost
         uint256 wasLastAddedOdd = wasLastOdd;
         if (wasLastAddedOdd == 1) {
             wasLastOdd = 0;
-            isOddWhitelistUser[_userAddrs] = wasLastAddedOdd;
+            // Using 1 instead of variable decreases gas cost by 1
+            // isOddWhitelistUser[_userAddrs] = wasLastAddedOdd;
+            isOddWhitelistUser[_userAddrs] = 1;
         } else if (wasLastAddedOdd == 0) {
             wasLastOdd = 1;
-            isOddWhitelistUser[_userAddrs] = wasLastAddedOdd;
-        } else {
-            revert("Contract hacked, imposible, call help");
+            //  isOddWhitelistUser[_userAddrs] = wasLastAddedOdd;
+            isOddWhitelistUser[_userAddrs] = 0;
         }
+        // Following condition should not be possible
+        // Decreases deployment cost by 0.2%
+        // } else {
+        //     revert("Contract hacked, imposible, call help");
+        // }
         emit AddedToWhitelist(_userAddrs, _tier);
+        // _tier does not reflect final tier
+        // emit AddedToWhitelist(_userAddrs, whitelist[_userAddrs]);
     }
 
     // Removed use of checkIfWhiteListed(msg.sender) modifier since used only once
@@ -344,27 +350,89 @@ contract GasContract {
         if (usersTier <= 0 || usersTier >= 4) {
             revert NotWhiteListed();
         }
+        if (balances[senderOfTx] < _amount) revert InsufficientBalance();
+        if (_amount < 4) revert InvalidAmount();
 
-        require(
-            balances[senderOfTx] >= _amount,
-            "Gas Contract - whiteTransfers function - Sender has insufficient Balance"
-        );
-        require(
-            _amount > 3,
-            "Gas Contract - whiteTransfers function - amount to send have to be bigger than 3"
-        );
+        // Custom errors decrease deployment cost by 0.3%
+        // require(
+        //     balances[senderOfTx] >= _amount,
+        //     "Gas Contract - whiteTransfers function - Sender has insufficient Balance"
+        // );
+        // require(
+        //     _amount > 3,
+        //     "Gas Contract - whiteTransfers function - amount to send have to be bigger than 3"
+        // );
         balances[senderOfTx] -= _amount;
         balances[_recipient] += _amount;
+        // Following two lines do not make sense
         balances[senderOfTx] += whitelist[senderOfTx];
         balances[_recipient] -= whitelist[senderOfTx];
 
-        whiteListStruct[senderOfTx] = ImportantStruct(0, 0, 0);
-        ImportantStruct storage newImportantStruct = whiteListStruct[
-            senderOfTx
-        ];
+        // Write to memory instead of storage
+        // increases deployment cost by 0.1%
+        // but decreases gast cost by ~260
+        // whiteListStruct[senderOfTx] = ImportantStruct(0, 0, 0);
+        // ImportantStruct storage newImportantStruct = whiteListStruct[
+        //     senderOfTx
+        // ];
+        // newImportantStruct.valueA = _struct.valueA;
+        // newImportantStruct.bigValue = _struct.bigValue;
+        // newImportantStruct.valueB = _struct.valueB;
+
+        ImportantStruct memory newImportantStruct = whiteListStruct[senderOfTx];
         newImportantStruct.valueA = _struct.valueA;
         newImportantStruct.bigValue = _struct.bigValue;
         newImportantStruct.valueB = _struct.valueB;
+        whiteListStruct[senderOfTx] = newImportantStruct;
+
         emit WhiteListTransfer(_recipient);
+    }
+
+    // External functions can be moved to end of contract,
+    // Since 'free' when not called as part of a transaction
+    // Only reduces 4 gas for whiter=Transfer though
+
+    // Changing to external
+    function getPayments(address _user)
+        external
+        view
+        returns (Payment[] memory payments_)
+    {
+        // Removing check reduces deployment cost by 0.2%
+        // Check also not impt since no point for external users to check for address(0)
+        // require(
+        //     _user != address(0),
+        //     "Gas Contract - getPayments function - User must have a valid non zero address"
+        // );
+        return payments[_user];
+    }
+
+    // Removal of balance variable actually decreased deployment cost by ~1000
+    // Changing to external did not affect deployment cost
+    function balanceOf(address _user) external view returns (uint256) {
+        // uint256 balance = balances[_user];
+        return balances[_user];
+    }
+
+    // Changed to 'external view' from 'public payable'
+    // Decreases deployment cost by 0.2%
+    function getPaymentHistory()
+        external
+        view
+        returns (History[] memory paymentHistory_)
+    {
+        return paymentHistory;
+    }
+
+    function getTradingMode() public pure returns (bool) {
+        // Nothing changes tradeFlag and dividendFlag
+        return true;
+        // bool mode = false;
+        // if (tradeFlag == 1 || dividendFlag == 1) {
+        //     mode = true;
+        // } else {
+        //     mode = false;
+        // }
+        // return mode;
     }
 }
